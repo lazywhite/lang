a7498551e18a441b47d6784f203fa1e7cb3ad202
lazywhite <346816483@qq.com>
2016-10-06 17:31:04 +0800

    add note for git

---
+++ backend/go/channel.go
@@ -0,0 +1,13 @@
+/*
+Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	message := make(chan string)
+	go func() { message <- "ping" }()
+	msg := <-message
+	fmt.Println(msg)
+}
