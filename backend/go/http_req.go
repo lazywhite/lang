90fc0496a44db8c59acb870d8ce411a4cb9379be
lazywhite <346816483@qq.com>
2016-10-29 13:26:43 +0800

    update for go

--- backend/go/http_req.go
+++ backend/go/http_req.go
@@ -4,7 +4,7 @@ import "net/http"
 import "fmt"
 
 func main() {
-	resp, err := http.Get("http://www.baidu.com")
+	resp, err := http.Head("http://www.baidu.com")
+	fmt.Println(resp.StatusCode)
 	fmt.Println(err)
-	fmt.Println(resp)
 }
