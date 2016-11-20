a8132e9ddabf1864954b2d1a852c1a7389926eca
lazywhite <346816483@qq.com>
2016-11-21 01:55:24 +0800

    update note for gradle

---
+++ backend/java/gradle_build/app/src/main/java/com/local/test/app/Main.java
@@ -0,0 +1,9 @@
+package com.local.test.app;
+import com.local.test.core.Utils;
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("hello from Web main");
+        Utils.greet();
+    }
+}
