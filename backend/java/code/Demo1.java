6400e52ccd0a8fd8f85278dda6a8af04137430a2
lazywhite <346816483@qq.com>
2017-05-15 23:51:24 +0800

    update for java

---
+++ backend/java/code/Demo1.java
@@ -0,0 +1,25 @@
+public class Demo1{
+
+    public static void main(String[] args) {
+        int i = 24, j = 32, h = 58;
+        switch (j - i) {
+            case 7:
+                h++;
+                System.out.println("case 7");
+            case 8:
+                h++;
+                System.out.println("case 8");
+            case 9:
+                h += 2;
+                System.out.println("case 9");
+            case 10:
+                h += 3;
+                System.out.println("case 10");
+            default:
+                h /= j;
+                System.out.println("case default");
+        }
+        System.out.println(h);
+    }
+
+}
---
+++ backend/java/code/basic/LeapYear.java
@@ -0,0 +1,20 @@
+package basic;
+import java.util.Scanner;
+/* 闰年计算规则
+ * 1. 能被4整除， 不能被100整除
+ * 2. 能被400整除
+ * 
+ */
+
+public class LeapYear {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please input year: ");
+		int m = sc.nextInt();
+		if ((m % 4 ==0 && m % 100 != 0)|| (m % 400 == 0)){
+			System.out.println("闰年");
+		}else{
+			System.out.println("平年");
+		}
+	}
+}
