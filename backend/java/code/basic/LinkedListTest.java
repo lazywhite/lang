764a36c56b5cf45c1abdfe699e2c0122fd83692e
white <346816483@qq.com>
2017-05-19 14:32:31 +0800

    update for treeSet, linkedList

--- backend/java/code/basic/LinkedListTest.java
+++ backend/java/code/basic/LinkedListTest.java
@@ -1,7 +1,9 @@
-
+import java.util.Collections;
 import java.util.LinkedList;
 /**
  * Created by white on 17/5/19.
+ * LinkedList 可以被当做堆栈, 队列, 或双端队列进行操作
+ * 每个元素除了存储自己的数据, 还包含前一个与后一个元素的引用
  */
 public class LinkedListTest {
     public static void main(String[] args) {
@@ -10,26 +12,32 @@ public class LinkedListTest {
         Student st2 = new Student(11, "lucy", 24);
         Student st3 = new Student(12, "nancy", 56);
         Student st4 = new Student(14, "jerry", 36);
+        Student st5 = new Student(15, "yank", 36);
         ll.add(st1);
         ll.add(st2);
         ll.add(st3);
         System.out.println(ll.toString());
 //        ll.remove(st3);
-//        System.out.println(ll.toString());
         System.out.println(ll.isEmpty());
         System.out.println(ll.getFirst());
         System.out.println(ll.getLast());
 //        System.out.println(ll.removeFirst());
 //        System.out.println(ll.removeLast());
-        System.out.println(ll.toString());
-        System.out.println(ll.size());
         ll.addLast(st4);
-        System.out.println(ll.size());
+        ll.addFirst(st5);
+//        ll.removeFirst();
+//        ll.removeLast();
         System.out.println(ll.element());
-        System.out.println(ll.pop());
+//        System.out.println(ll.pop());
         System.out.println(ll.size());
-        System.out.println(ll.peek());
+        System.out.println(ll);
+        System.out.println("peek first element: " + ll.peekFirst());//retrive but not remove the first element
+        System.out.println("peek last element: " + ll.peekLast());//retrive but not remove the last element
+
         System.out.println(ll.toString());
+        System.out.println(ll.get(0));//get by index
+        System.out.println(Collections.max(ll)); //get max  element
+        System.out.println(Collections.min(ll)); //get min element
 
     }
 }
