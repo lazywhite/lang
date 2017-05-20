53cb899c63d7b3eb02e2f2142e236fe88e95b0a1
white <346816483@qq.com>
2017-05-20 16:15:49 +0800

    super keyword usage

--- backend/java/code/oo/Animal.java
+++ backend/java/code/oo/Animal.java
@@ -2,10 +2,21 @@
 public class Animal {
     String name;
     int age;
+
     public Animal(){
     }
+
     public Animal(String name, int age){
         this.name = name;
         this.age = age;
+    }
+
+    public void move(){
+        System.out.println("animal moving");
+    }
+
+    @Override
+    public String toString(){
+        return this.name + " " + this.age + " ";
     }
 }
