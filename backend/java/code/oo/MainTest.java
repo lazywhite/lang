be8c62e2656041d5d1f26c035c55c9d51dfd3cda
lazywhite <346816483@qq.com>
2017-05-20 16:15:49 +0800

    super keyword usage

--- backend/java/code/oo/MainTest.java
+++ backend/java/code/oo/MainTest.java
@@ -4,8 +4,11 @@ public class MainTest {
         Animal a = new Animal();
         System.out.println(a.name);
         System.out.println(a.age);
+        Mammal m = new Mammal("good", 10, "Mouse");
+        m.move();
 
-
+        System.out.println(m.getCategory());
+        System.out.println(m.toString());
 
     }
 }
