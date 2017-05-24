92d2a2ef9e30f58e609447e252a61bad2ac42bf6
lazywhite <346816483@qq.com>
2017-05-24 16:05:14 +0800

    java decimal

---
+++ backend/java/code/basic/DecimalTest.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+
+/**
+ * BigDecimal用于精确浮点数运算
+ */
+public class DecimalTest{
+    public static void main(String[] args){
+        BigDecimal bd = new BigDecimal(10.1);
+        BigDecimal bd2 = new BigDecimal(1.3);
+        BigDecimal result = bd.add(bd2);
+        System.out.println(result.doubleValue());
+        System.out.println(result.floatValue());
+    }
+}
