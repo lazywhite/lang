46ca6c12fc592f9ca8535c5010b2c731b5ad5570
white <346816483@qq.com>
2017-05-25 12:02:21 +0800

    java.util.Random

---
+++ backend/java/code/basic/RandomTest.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class RandomTest{
+    public static void main(String[] args){
+        System.out.println("------random------");
+        Random r = new Random();
+        System.out.println(r.nextFloat());
+        System.out.println(r.nextDouble());
+        System.out.println(r.nextBoolean());
+        System.out.println(r.nextInt(100));
+        byte[] bbbb = new byte[10];
+        r.nextBytes(bbbb);
+        System.out.println(Arrays.toString(bbbb));
+    }
+}
