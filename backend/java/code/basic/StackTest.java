1f8983253d7f0ec89540db306d232eeabe8bc403
lazywhite <346816483@qq.com>
2017-05-26 16:43:29 +0800

    java stack

---
+++ backend/java/code/basic/StackTest.java
@@ -0,0 +1,18 @@
+import java.util.Stack;
+/**
+ * Created by white on 17/5/26.
+ * stack: 栈, 后进先出
+ */
+public class StackTest {
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push(100);
+        st.push("good");
+        st.push(200);
+        st.pop();
+        if(!st.empty()) {
+            System.out.println(st.peek());//查看栈顶部的对象
+            System.out.println(st.capacity());
+        }
+    }
+}
