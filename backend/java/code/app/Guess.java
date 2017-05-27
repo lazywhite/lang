955f22b1ee4e96601159c8564434c84ebb2c8793
lazywhite <346816483@qq.com>
2017-05-27 12:14:25 +0800

    java猜数字游戏

---
+++ backend/java/code/app/Guess.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Guess{
+    public static void main(String[] args){ 
+        guess();
+    }
+    public static void guess(){
+        int total=0;
+        int num;
+        Random r = new Random();
+        int pivot  = r.nextInt(100);
+        Scanner sc = new Scanner(System.in);
+        do{
+            total++;
+            System.out.println("please input your number between 0 and 100");
+            num = sc.nextInt(); 
+            if (num == pivot){
+                System.out.println("You are right, num is: " + pivot);
+                System.out.println("total num: " + total);
+                break;
+            }
+            if(num > pivot){
+                System.out.println("too big");
+            }else{
+                System.out.println("too small");
+            }
+            continue;
+
+        }while(true);
+    }
+}
