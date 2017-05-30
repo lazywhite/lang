6daa0042a22c7f8a37a94dc39341148451ed969c
white <346816483@qq.com>
2017-05-30 18:05:34 +0800

    update java string usage

--- backend/java/code/basic/StringTest.java
+++ backend/java/code/basic/StringTest.java
@@ -2,33 +2,33 @@ import java.util.Arrays;
 
 /**
  * String 类型为immutable, thread safe
+ * 字符串池
  * StringBuilder类型是可变的， 在字符串拼接，修改时效率比较高；但不是线程安全的
  * StringBuffer类型跟StringBuilder特性基本相同， 但是thread safe的
  */
 public class StringTest{
     public static void main(String[] args){
-        String st = "test01";
+        System.out.println("========");
         StringBuilder strBuilder = new StringBuilder();
         for(int i=0;i<=10;i++){
             strBuilder.append(i);
         }
         System.out.println(strBuilder);
+
         StringBuffer strBuffer = new StringBuffer();
         strBuffer.append("test string");
         strBuffer.append("test string");
         System.out.println(strBuffer);
 
-
+        /*
+        * difference between equals and ==
+         */
         String str = "new string";
         String str1 = new String("new string");
         System.out.println(str == str1); // -->false
         System.out.println(str.equals(str1));// -->true
         System.out.println(str.equalsIgnoreCase(str1));
         System.out.println(str.startsWith("new"));
-
-        StringBuffer strBuf = new StringBuffer("good");
-        strBuf.append(" morning");
-        System.out.println(strBuf);
 
 
         // String methods
@@ -43,7 +43,5 @@ public class StringTest{
 
         String newStr = testStr.replaceAll("test", "local"); //origin string is not modified
         System.out.println(newStr);
-
-
     }
 }
