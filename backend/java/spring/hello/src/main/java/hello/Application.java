2a299078996bee89a083d4f0fd63df018d8e3d11
white <346816483@qq.com>
2017-06-04 01:06:22 +0800

    code for spring

---
+++ backend/java/spring/hello/src/main/java/hello/Application.java
@@ -0,0 +1,16 @@
+package hello;
+
+/**
+ * Created by white on 17/6/3.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ backend/java/spring/scheduler/src/main/java/hello/Application.java
@@ -0,0 +1,18 @@
+package hello;
+
+/**
+ * Created by white on 17/6/3.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Application {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class);
+    }
+}
+
