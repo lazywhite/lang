623210458d5fe8a0a262bd11ec0c39cebfa8e1ed
lazywhite <346816483@qq.com>
2017-06-07 14:20:19 +0800

    update for java inner class

---
+++ backend/java/code/oo/inner/TestLocalInner.java
@@ -0,0 +1,31 @@
+/**
+ * Created by white on 17/5/20.
+ * 局部内部类
+ */
+class PartOuter{
+    static int x = 10;
+    int y = 20;
+    public void run(){
+        class Inner{
+            int z = 20;
+            int w = 30;
+//            static int w = 30;  //无法定义
+            public void test(){
+                System.out.println(x);
+                System.out.println(y);
+                System.out.println(z);
+                System.out.println(w);
+            }
+        }
+
+        Inner i = new Inner();
+        i.test();
+    }
+}
+public class TestLocalInner {
+    public static void main(String[] args) {
+        PartOuter p = new PartOuter();
+        p.run();
+    }
+
+}
--- backend/java/code/oo/inner/TestOuter.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by white on 17/5/18.
- */
-public class TestOuter {
-    public static void main(String[] args){
-        MemberOuter outer = new MemberOuter();
-        outer.outerFunc();
-    }
-}
