02982f1918ef16020a1dc42c56eeab7824d3df30
white <346816483@qq.com>
2017-06-08 10:29:33 +0800

    java parse data from string

--- backend/java/code/tool/DateTest.java
+++ backend/java/code/tool/DateTest.java
@@ -1,11 +1,11 @@
 import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class DateTest {
-	public static void main(String[] args){
-		Date d = new Date();
-		System.out.println(d.getClass().getName());
-		System.out.println(d.getTime());
-		System.out.print(d.toString());
+    public static void main(String[] args){
+        Date d = new Date();
+        System.out.println(d.getTime());
+        System.out.print(d.toString());
 
 
         DateUtils du = new DateUtils();
@@ -15,6 +15,18 @@ public class DateTest {
 
         Date d1 = new Date(1412654676572L);
         Date d2 = new Date(1412654776572L);
-		System.out.println(du.isSameWeekDates(d1, d2));
-	}
+        System.out.println(du.isSameWeekDates(d1, d2));
+
+        //parse string to get Date
+        String str = "2012-02-10";
+        Date d3;
+        try{
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd");        
+            d3 = sdf.parse(str);
+            System.out.println(d3.getTime());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
 }
