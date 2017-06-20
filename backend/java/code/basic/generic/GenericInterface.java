d60398d7646aa67f232680081eb88b20c845d6a4
white <346816483@qq.com>
2017-06-20 22:57:55 +0800

    java generic

--- backend/java/code/basic/generic/GenericInterface.java
+++
@@ -1,9 +0,0 @@
-package oo.generic;
-
-/**
- * Created by white on 17/5/19.
- */
-public interface InterfaceTest<T> {
-    public T getName();
-    String name = "good";//接口中的变量默认为public static final类型
-}
