d1a3357ddbba75fd9935cffb9fe9d5a61328834d
lazywhite <346816483@qq.com>
2017-06-20 22:57:55 +0800

    java generic

---
+++ backend/java/code/basic/generic/GenericClass.java
@@ -0,0 +1,34 @@
+/*
+ * GenericClass.java
+ * Copyright (C) 2017 white <white@Whites-Mac-Air.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+interface Throw<T>{
+    public T throwOut();
+}
+
+class Service<T> implements Throw<T>{
+    public T item;
+
+    @Override 
+    public T throwOut(){
+        return this.item;
+    }
+
+    public Service(T item){
+        this.item = item;
+    }
+
+}
+public class GenericClass {
+     
+    public static void main(String[] args){
+        Service<String> a = new Service<>("hello");
+        System.out.println(a.throwOut());
+        Service<Integer> b = new Service<>(10);
+        System.out.println(b.throwOut());
+    }
+}
+
---
+++ backend/java/code/basic/generic/GenericMethod.java
@@ -0,0 +1,19 @@
+/*
+ * GenericMethod.java
+ * Copyright (C) 2017 white <white@Whites-Mac-Air.local>
+ *
+ * Distributed under terms of the MIT license.
+ */
+class ArrayAlg{
+    public static <T> T getMiddle(T... a){
+        return a[a.length/2];
+    }
+}
+
+public class GenericMethod {
+    public static void main(String[] args){
+        String middle = ArrayAlg.<String>getMiddle("bob", "john", "qoo");
+        System.out.println(middle);
+    }
+}
+
