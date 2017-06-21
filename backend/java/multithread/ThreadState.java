1860aaaa1598a1b9e207107996a704337a74aae3
white <346816483@qq.com>
2017-06-21 20:07:24 +0800

    java多线程通信及reentrantlock简单使用

---
+++ backend/java/multithread/ThreadState.java
@@ -0,0 +1,96 @@
+/*
+ * ThreadState.java
+ * Copyright (C) 2017 white <white@Whites-Mac-Air.local>
+ *
+ * Distributed under terms of the MIT license.
+ * 线程的状态
+ *      Thread.State(Enum)
+ *          NEW
+ *          RUNNABLE
+ *          TERMINATED
+ *          TIMED WAITING
+ *          WAITING
+ *          BLOCKED
+ */
+class MyService{
+    synchronized static public void service(){
+        try{
+            System.out.println(Thread.currentThread().getName() + "进入了业务方法");
+            Thread.sleep(1000);
+        }catch(InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+class Bthread extends Thread{
+    @Override
+    public void run(){
+        MyService.service();
+    }
+}
+
+class Cthread extends Thread{
+    @Override
+    public void run(){
+        String lock = new String();
+        System.out.println("before sync block");
+        synchronized (lock){
+            System.out.println("entered sync block");
+            try{
+                lock.wait();
+                System.out.println("after wait");//线程释放了对象锁, 在被notify之前, 不会执行到这里
+
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Athread extends Thread{
+    public Athread(){
+        System.out.println("线程初始化时的状态: " + Thread.currentThread().getState());
+    }
+    @Override
+    public void run(){
+        System.out.println("线程运行时的状态: " + Thread.currentThread().getState());
+        try{
+            Thread.sleep(200);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+public class ThreadState {
+
+    public static void main(String[] args){
+        try{
+            Cthread ct = new Cthread();
+            ct.start();
+            Athread a = new Athread();
+            System.out.println("线程新建后的状态: " + a.getState());
+            a.start();
+            Thread.sleep(100);
+            System.out.println("线程运行时的状态: " + a.getState());
+            Thread.sleep(300);
+            System.out.println("线程结束后的状态: " + a.getState());
+
+            Bthread b = new Bthread();
+            b.setName("b");
+            b.start();
+            Thread.sleep(100);
+            Bthread c = new Bthread();
+            c.setName("c");
+            c.start();
+            Thread.sleep(100);
+            System.out.println("等待其他线程执行同步方法时的线程状态: " + c.getState());
+            System.out.println("等待其他线程通知的线程状态: " + ct.getState());
+            //由于线程ct一直处在WAITING状态, 主进程无法退出
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
+
--- backend/java/multithread/note.md
+++ backend/java/multithread/note.md
@@ -1,13 +1,34 @@
-1. 线程处于就绪和运行状态, isAlive()返回true, 其他返回false
-2. Thread.currentThread().getId()返回线程的唯一标示
-3. 终止正在运行的线程的方法
-    1. 使用退出标志, 使线程正常退出, 也就是当run()方法完成后线程终止
-    2. 使用stop方法强行终止, 但不推荐, 跟suspend(), resume()一样已经废弃
-    3. 使用interrupt()终止
-4. Thread.sleep()需要捕获InterruptedException, 线程sleep()中被interrupt()
-5. stop()会强制停止线程, 导致清理工作无法完成, 并且会自动解锁被锁定的对象, 导致数据不一致
-6. yield()的作用是放弃当前cpu资源, 让给其他任务去执行, 但放弃的时间不确定, 有可能刚放弃又得到cpu时间片
-7. java中, 线程的优先级具有继承性, 如果A线程启动B线程, 则B线程的优先级跟A是一样的
-8. 高优先级的线程更容易得到cpu时间片, 跟谁先执行完无关
-9. 线程分为两种: 用户线程, 守护线程
-10. 当用户线程全部退出, 则守护线程将自动销毁
+线程处于就绪和运行状态, isAlive()返回true, 其他返回false
+Thread.currentThread().getId()返回线程的唯一标示
+终止正在运行的线程的方法
+    使线程正常退出
+    使用stop方法强行终止, 但不推荐, 跟suspend(), resume()一样已经废弃
+    使用interrupt()终止
+        return 
+        raise InterruptException
+Thread.sleep()需要捕获InterruptedException
+如果线程正在执行sleep(), 此时对其调用interrupt(), 则线程直接进入catch语句, 并且isInterrupted()返回false
+stop()会强制停止线程
+    导致清理工作无法完成
+    会自动释放掉获得的锁
+yield()的作用是放弃当前cpu资源, 让给其他任务去执行, 但放弃的时间不确定, 有可能刚放弃又得到cpu时间片
+java中, 线程的优先级具有继承性, 如果A线程启动B线程, 则B线程的优先级跟A是一样的
+高优先级的线程更容易得到cpu时间片, 仅此而已
+线程分为两种: 用户线程, 守护线程, 当用户线程全部退出, 则守护线程将自动销毁
+进程退出, 则所有线程也退出
+因为方法内部的变量是私有的, 所以局部变量永远是线程安全的, 只有实例变量才会牵扯到线程安全
+sychronized 取得的锁是基于对象的, 只有多个线程访问通过一个对象才生效, 有多少个对象会产生多少个对应的锁
+Obj具有sync methodA, sync methodB, async methodC, 线程A执行methodA时, 线程B可执行methodC, 阻塞在methodB
+通过synchronized获取的对象锁是可重入的, sync方法内部可调用自身其他的sync方法
+子类可以通过sync可重入锁执行父类的sync 方法
+当线程执行出现异常时, 会自动释放所持有的锁
+synchronized(this){}可以修饰方法内的一段代码, 其他部分仍是async执行的
+如果线程A正在执行Obj的一个方法内的sync(this)代码块, 则线程B对obj所有其他方法中的sync(this)代码段的访问将被阻塞
+synchronized可以修饰静态方法,普通方法, 任意对象,this, class ,修饰静态方法相当于对class加锁, 修饰普通方法是对对象加锁, 修饰this也是对对象加锁
+类锁可以对所有属于此类的对象生效
+因为jvm有String常量池, 所以大多数情况下同步代码块不适用String作为锁对象
+
+如果一个对象有两个sync方法, 其中一个是死循环, 则其他线程永远无法执行另一个方法, 解决方法是使用同步块
+jps jstack -l <pid>可检测是否存在死锁现象
+a.join() 当前线程会等待a线程执行完毕才继续执行, 底层是wait()
+a.join(time) 只等待一段时间
