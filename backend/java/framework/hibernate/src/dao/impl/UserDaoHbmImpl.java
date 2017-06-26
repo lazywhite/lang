5821504c93fd8b190c4ae42147a225d12bfbf1a6
lazywhite <346816483@qq.com>
2017-06-26 16:48:54 +0800

    hibernate简单使用

---
+++ backend/java/framework/hibernate/src/dao/impl/UserDaoHbmImpl.java
@@ -0,0 +1,95 @@
+package dao.impl;
+
+import dao.UserDao;
+import entity.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import util.HibernateUtil;
+
+import java.util.List;
+
+/**
+ * Created by white on 17/6/26.
+ *
+ * 增 删 改 必须要添加事务
+ */
+public class UserDaoHbmImpl implements UserDao {
+    @Override
+    public User findById(Integer id) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSession();
+            return session.get(User.class, id);
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            session.close();
+        }
+        return null;
+    }
+
+    @Override
+    public void update(User u) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSession();
+            Transaction tx = session.beginTransaction();
+            session.update(u);
+            tx.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            session.close();
+        }
+
+    }
+
+    @Override
+    public void delete(User u) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            session.delete(u);
+            transaction.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            session.close();
+        }
+
+    }
+
+    @Override
+    public void create(User u) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSession();
+            Transaction transaction = session.beginTransaction();
+            session.save(u);
+            transaction.commit();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            session.close();
+        }
+
+    }
+
+    @Override
+    public List<User> getAll(){
+        Session session = null;
+        try {
+            session = HibernateUtil.getSession();
+            Query q = session.createQuery("from User");//from 类名
+            return q.list();
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally{
+            session.close();
+        }
+        return null;
+    }
+
+}
---
+++ backend/java/framework/hibernate/src/util/HibernateUtil.java
@@ -0,0 +1,19 @@
+package util;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class HibernateUtil {
+    private static SessionFactory sessionFactory;
+    static{
+        Configuration conf = new Configuration();
+        conf.configure("hibernate.cfg.xml");
+        sessionFactory =  conf.buildSessionFactory();
+    }
+    public static Session getSession(){
+        return sessionFactory.openSession();
+    }
+
+}
