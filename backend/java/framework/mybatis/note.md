5d1d5e0bda2aafed2760a4ff444f7ce7c56bbbae
lazywhite <346816483@qq.com>
2017-07-03 19:51:41 +0800

    mybatis简单使用

---
+++ backend/java/framework/mybatis/note.md
@@ -0,0 +1,14 @@
+构建sessionFactory的方法
+    1. mybatis-config.xml
+    2. java代码
+
+jdbc事务管理, 默认自动提交, 可以关闭自动提交
+直接在session上提交事务
+
+mapper.xml
+    select
+        @id
+        @resultType
+        @parameterType  完整类名或别名
+
+multi datasource
---
+++ backend/java/framework/mybatis/src/main/java/local/mybatis/util/MybatisUtil.java
@@ -0,0 +1,31 @@
+package local.mybatis.util;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by white on 17/7/3.
+ */
+public class MybatisUtil {
+    private static SqlSessionFactory sessionFactory;
+    static{
+        String resource="mybatis-config.xml";
+        InputStream is = null;
+        try {
+            is = Resources.getResourceAsStream(resource);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        sessionFactory = new SqlSessionFactoryBuilder().build(is);
+    }
+
+    public static SqlSessionFactory getSessionFactory(){
+        return sessionFactory;
+    }
+
+
+}
