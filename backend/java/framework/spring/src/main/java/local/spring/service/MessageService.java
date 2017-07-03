621feddb35f3a0a80f5adff6c2d9a9918fd1a1f0
white <346816483@qq.com>
2017-07-03 19:31:41 +0800

    cleanup spring

---
+++ backend/java/framework/spring/src/main/java/local/spring/service/MessageService.java
@@ -0,0 +1,12 @@
+package local.spring.service;
+/*
+ * MessageService.java
+ * Copyright (C) 2017 white <white@localhost>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+public interface MessageService {
+    String getMessage();     
+}
+
