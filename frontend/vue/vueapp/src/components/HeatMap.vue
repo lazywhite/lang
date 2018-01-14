ed9a1aefb2988f7835d09962fe822482900e45de
lazywhite <346816483@qq.com>
2018-01-14 16:50:26 +0800

    update vue note

---
+++ frontend/vue/vueapp/src/components/HeatMap.vue
@@ -0,0 +1,34 @@
+<template>
+  <ve-heatmap :data="chartData" :chartSettings="chartSettings" :visual-map="chartVisualMap" :grid="chartGrid"></ve-heatmap>
+</template>
+
+<script>
+import VeHeatMap from 'v-charts/lib/heatmap';
+export default {
+    components:{
+        've-heatmap': VeHeatMap,
+    },
+    created: function () {
+      this.chartSettings = {
+      };
+      this.chartGrid = {
+        right: 200,
+      }
+      this.chartVisualMap = {
+        //min: 0,
+        max: 10,
+        type: 'piecewise',
+        right: 0,
+        top: '50%'
+      },
+      this.chartData = {
+        columns: ['cab', 'unum', 'value'],
+        rows: [
+            {cab: '201', unum: 3, value: 10},
+            {cab: '202', unum: 4, value: 20},
+            {cab: '203', unum: 6, value: 50},
+        ]
+      }
+    }
+  }
+</script>
