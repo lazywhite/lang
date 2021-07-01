4a86f4c6cd86105eca017eb8a034be794b10139b
lazywhite <346816483@qq.com>
2021-07-01 10:26:47 +0800

    add unix socket

--- backend/go/code/grpc/cmd/client.go
+++ backend/go/code/grpc/cmd/client.go
@@ -5,22 +5,31 @@ import (
 	"fmt"
 	"google.golang.org/grpc"
 	"log"
+	"net"
 	"time"
 
 	"demo/pkg/pb"
 )
 
-const(
-	addr = "localhost:9090"
+const (
+	sockPath = "/tmp/sample.sock"
 )
 
 func main(){
-	conn, err := grpc.Dial(addr, grpc.WithInsecure(), grpc.WithBlock())
-	if err != nil{
-		log.Fatal("failed to connect server")
-	}
+	//1. for tcp
+	//conn, err := grpc.Dial("localhost:9090", grpc.WithInsecure(), grpc.WithBlock())
+
+	//2. for unix socket
+	conn, err := grpc.Dial(sockPath,
+		grpc.WithInsecure(),
+		grpc.WithContextDialer(func(context.Context, string)(net.Conn, error){
+			addr, err := net.ResolveUnixAddr("unix", sockPath)
+			conn, err := net.DialUnix("unix", nil, addr)
+			return conn, err
+		}))
 	defer conn.Close()
 
+	//c := pb.NewGreetServiceClient(conn)
 	c := pb.NewGreetServiceClient(conn)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
--- backend/go/code/grpc/cmd/server.go
+++ backend/go/code/grpc/cmd/server.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"demo/pkg/pb"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"google.golang.org/grpc"
 )
@@ -23,15 +25,43 @@ func (s *SampleServer) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.
 }
 
 
+const (
+	unixSockPath = "/tmp/sample.sock"
+)
+
+func logGRPC(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	fmt.Printf("%v\n", ctx)
+	fmt.Printf("%v\n", req)
+	fmt.Printf("%v\n", info)
+	fmt.Printf("%v\n", handler)
+	resp, err := handler(ctx, req)
+	if err != nil {
+		log.Fatalf("GRPC error: %v\n", err)
+	}
+	return resp, err
+}
 
 func main(){
-    lis, err := net.Listen("tcp", ":9090")
+	//1. for tcp
+    //lis, err := net.Listen("tcp", ":9090")
+
+	//2. for unix socket
+	if err := os.Remove(unixSockPath); err != nil && !os.IsNotExist(err){
+		log.Fatal("remove socket error")
+	}
+	lis, err := net.Listen("unix", unixSockPath)
+
 	if err != nil{
 		log.Fatal("bind port error")
 	}
-	s := grpc.NewServer()
+
+	opts := []grpc.ServerOption{
+		grpc.UnaryInterceptor(logGRPC),
+	}
+	s := grpc.NewServer(opts...)
 	pb.RegisterGreetServiceServer(s, &SampleServer{})
 
+	fmt.Println("serving...")
 	if err = s.Serve(lis);err != nil{
 		log.Fatal("serve error, %v", err)
 	}
